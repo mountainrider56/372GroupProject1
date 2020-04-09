2a6acca2bc9792d62561a0c084ed17796045ce83
mountainrider56 <63173933+mountainrider56@users.noreply.github.com>
2020-04-08 19:57:23 -0500

    Update Library.java

--- Library.java
+++ Library.java
@@ -1,23 +1,8 @@
 
 /**
- * 
- * @author Brahma Dathan and Sarnath Ramnath
- * @Copyright (c) 2010
- 
- * Redistribution and use with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   - the use is for academic purpose only
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   - Neither the name of Brahma Dathan or Sarnath Ramnath
- *     may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * The authors do not make any claims regarding the correctness of the code in this module
- * and are not responsible for any loss or damage resulting from its use.  
+ *  
  */
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -29,17 +14,9 @@ import java.util.Iterator;
 
 public class Library implements Serializable {
 	private static final long serialVersionUID = 1L;
-	public static final int BOOK_NOT_FOUND = 1;
-	public static final int BOOK_NOT_ISSUED = 2;
-	public static final int BOOK_HAS_HOLD = 3;
-	public static final int BOOK_ISSUED = 4;
-	public static final int HOLD_PLACED = 5;
-	public static final int NO_HOLD_FOUND = 6;
-	public static final int OPERATION_COMPLETED = 7;
-	public static final int OPERATION_FAILED = 8;
-	public static final int NO_SUCH_MEMBER = 9;
+	
 	private Catalog catalog;
-	private MemberList memberList;
+	private MemberList customerList;
 	private static Library library;
 
 	/**
@@ -47,8 +24,7 @@ public class Library implements Serializable {
 	 * objects
 	 */
 	private Library() {
-		catalog = Catalog.instance();
-		memberList = MemberList.instance();
+		
 	}
 
 	/**
@@ -57,259 +33,45 @@ public class Library implements Serializable {
 	 * @return the singleton object
 	 */
 	public static Library instance() {
-		if (library == null) {
-			MemberIdServer.instance(); // instantiate all singletons
-			return (library = new Library());
-		} else {
-			return library;
-		}
+
 	}
 
-	/**
-	 * Organizes the operations for adding a book
-	 * 
-	 * @param title  book title
-	 * @param author author name
-	 * @param id     book id
-	 * @return the Book object created
-	 */
-	public Book addBook(String title, String author, String id) {
-		Book book = new Book(title, author, id);
-		if (catalog.insertBook(book)) {
-			return (book);
+
+	public Appliance addAppliance(_) {
+		
+	}
+
+
+	public Member addCustomer(String name, String phone) {
+		Customer customer = new Customer(name, phone);
+		if (customerList.insertCustomer(customer)) {
+			return (customer);
 		}
 		return null;
 	}
 
-	/**
-	 * Organizes the operations for adding a member
-	 * 
-	 * @param name    member name
-	 * @param address member address
-	 * @param phone   member phone
-	 * @return the Member object created
-	 */
-	public Member addMember(String name, String address, String phone) {
-		Member member = new Member(name, address, phone);
-		if (memberList.insertMember(member)) {
-			return (member);
-		}
-		return null;
+
+
+
+	public Customer searchCustomer(String customerId) {
+		return customerList.search(customerId);
 	}
 
-	/**
-	 * Organizes the placing of a hold
-	 * 
-	 * @param memberId member's id
-	 * @param bookId   book's id
-	 * @param duration for how long the hold should be valid in days
-	 * @return indication on the outcome
-	 */
-	public int placeHold(String memberId, String bookId, int duration) {
-		Book book = catalog.search(bookId);
-		if (book == null) {
-			return BOOK_NOT_FOUND;
-		}
-		if (book.getBorrower() == null) {
-			return BOOK_NOT_ISSUED;
-		}
-		Member member = memberList.search(memberId);
-		if (member == null) {
-			return NO_SUCH_MEMBER;
-		}
-		Hold hold = new Hold(member, book, duration);
-		book.placeHold(hold);
-		member.placeHold(hold);
-		return HOLD_PLACED;
+
+	public Customer processBacklog(String _) {
+
 	}
 
-	/**
-	 * Searches for a given member
-	 * 
-	 * @param memberId id of the member
-	 * @return true iff the member is in the member list collection
-	 */
-	public Member searchMembership(String memberId) {
-		return memberList.search(memberId);
+
+	public int removeBacklog(String _, String _) {
+		
 	}
 
-	/**
-	 * Processes holds for a single book
-	 * 
-	 * @param bookId id of the book
-	 * @return the member who should be notified
-	 */
-	public Member processHold(String bookId) {
-		Book book = catalog.search(bookId);
-		if (book == null) {
-			return null;
-		}
-		Hold hold = book.getNextHold();
-		if (hold == null) {
-			return null;
-		}
-		hold.getMember().removeHold(bookId);
-		hold.getBook().removeHold(hold.getMember().getId());
-		return hold.getMember();
-	}
+	
 
-	/**
-	 * Removes a hold for a specific book and member combincation
-	 * 
-	 * @param memberId id of the member
-	 * @param bookId   book id
-	 * @return result of the operation
-	 */
-	public int removeHold(String memberId, String bookId) {
-		Member member = memberList.search(memberId);
-		if (member == null) {
-			return (NO_SUCH_MEMBER);
-		}
-		Book book = catalog.search(bookId);
-		if (book == null) {
-			return (BOOK_NOT_FOUND);
-		}
-		return member.removeHold(bookId) && book.removeHold(memberId) ? OPERATION_COMPLETED : NO_HOLD_FOUND;
-	}
-
-	/*
-	 * Removes all out-of-date holds
-	 */
-	private void removeInvalidHolds() {
-		for (Iterator catalogIterator = catalog.getBooks(); catalogIterator.hasNext();) {
-			for (Iterator iterator = ((Book) catalogIterator.next()).getHolds(); iterator.hasNext();) {
-				Hold hold = (Hold) iterator.next();
-				if (!hold.isValid()) {
-					hold.getBook().removeHold(hold.getMember().getId());
-					hold.getMember().removeHold(hold.getBook().getId());
-				}
-			}
-		}
-	}
-
-	/**
-	 * Organizes the issuing of a book
-	 * 
-	 * @param memberId member id
-	 * @param bookId   book id
-	 * @return the book issued
-	 */
-	public Book issueBook(String memberId, String bookId) {
-		Book book = catalog.search(bookId);
-		if (book == null) {
-			return (null);
-		}
-		if (book.getBorrower() != null) {
-			return (null);
-		}
-		Member member = memberList.search(memberId);
-		if (member == null) {
-			return (null);
-		}
-		if (!(book.issue(member) && member.issue(book))) {
-			return null;
-		}
-		return (book);
-	}
-
-	/**
-	 * Renews a book
-	 * 
-	 * @param bookId   id of the book to be renewed
-	 * @param memberId member id
-	 * @return the book renewed
-	 */
-	public Book renewBook(String bookId, String memberId) {
-		Book book = catalog.search(bookId);
-		if (book == null) {
-			return (null);
-		}
-		Member member = memberList.search(memberId);
-		if (member == null) {
-			return (null);
-		}
-		if ((book.renew(member) && member.renew(book))) {
-			return (book);
-		}
-		return (null);
-	}
-
-	/**
-	 * Returns an iterator to the books issued to a member
-	 * 
-	 * @param memberId member id
-	 * @return iterator to the collection
-	 */
-	public Iterator getBooks(String memberId) {
-		Member member = memberList.search(memberId);
-		if (member == null) {
-			return (null);
-		} else {
-			return (member.getBooksIssued());
-		}
-	}
-
-	/**
-	 * Removes a specific book from the catalog
-	 * 
-	 * @param bookId id of the book
-	 * @return a code representing the outcome
-	 */
-	public int removeBook(String bookId) {
-		Book book = catalog.search(bookId);
-		if (book == null) {
-			return (BOOK_NOT_FOUND);
-		}
-		if (book.hasHold()) {
-			return (BOOK_HAS_HOLD);
-		}
-		if (book.getBorrower() != null) {
-			return (BOOK_ISSUED);
-		}
-		if (catalog.removeBook(bookId)) {
-			return (OPERATION_COMPLETED);
-		}
-		return (OPERATION_FAILED);
-	}
-
-	/**
-	 * Returns a single book
-	 * 
-	 * @param bookId id of the book to be returned
-	 * @return a code representing the outcome
-	 */
-	public int returnBook(String bookId) {
-		Book book = catalog.search(bookId);
-		if (book == null) {
-			return (BOOK_NOT_FOUND);
-		}
-		Member member = book.returnBook();
-		if (member == null) {
-			return (BOOK_NOT_ISSUED);
-		}
-		if (!(member.returnBook(book))) {
-			return (OPERATION_FAILED);
-		}
-		if (book.hasHold()) {
-			return (BOOK_HAS_HOLD);
-		}
-		return (OPERATION_COMPLETED);
-	}
-
-	/**
-	 * Returns an iterator to the transactions for a specific member on a certain
-	 * date
-	 * 
-	 * @param memberId member id
-	 * @param date     date of issue
-	 * @return iterator to the collection
-	 */
-	public Iterator getTransactions(String memberId, Calendar date) {
-		Member member = memberList.search(memberId);
-		if (member == null) {
-			return (null);
-		}
-		return member.getTransactions(date);
+	
+	public Iterator getPurchase(_) {
+		
 	}
 
 	/**
@@ -343,7 +105,7 @@ public class Library implements Serializable {
 			FileOutputStream file = new FileOutputStream("LibraryData");
 			ObjectOutputStream output = new ObjectOutputStream(file);
 			output.writeObject(library);
-			output.writeObject(MemberIdServer.instance());
+			output.writeObject(CustomerIdServer.instance());
 			file.close();
 			return true;
 		} catch (IOException ioe) {
@@ -358,6 +120,6 @@ public class Library implements Serializable {
 	 */
 	@Override
 	public String toString() {
-		return catalog + "\n" + memberList;
+		return catalog + "\n" + customerList;
 	}
 }
