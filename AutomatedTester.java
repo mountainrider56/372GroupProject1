ae2156882a48a13fe91734ed9742cf39a5ef9d09
aaronzech <aaronzech@gmail.com>
2020-04-14 15:59:41 -0500

    Update AutomatedTester.java

--- AutomatedTester.java
+++ AutomatedTester.java
@@ -23,10 +23,11 @@ public class AutomatedTester {
 	 * Tests Member creation.
 	 */
 	public void test() {
-		String[] names = { "n1", "n2", "n3" };
-		String[] addresses = { "a1", "a2", "a3" };
-		String[] phones = { "p1", "p2", "p3" };
-		Member[] members = new Member[3];
+		String[] names = { "Sam", "Jane", "Belle","Frank","Joe","John","Jill","Francis","Roma" };
+		String[] addresses = { "a1", "a2", "a3" ,"a4","a5","a6","a7","a8","a9"};
+		String[] phones = { "651-555-4444", "547-888-1558", "155-555-5555","651-222-4222","651-785-7508","651-758-5888",
+				  "594-112-2310","542-432-3333","651-888-0010"};
+		Member[] members = new Member[9];
 		for (int count = 0; count < names.length; count++) {
 			members[count] = library.addMember(names[count], addresses[count], phones[count]);
 			assert members[count].getName().equals(names[count]);
