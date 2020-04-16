717bbda9f3f8cf7b8b0b89a4b66d1ea1a13caea0
mountainrider56 <63173933+mountainrider56@users.noreply.github.com>
2020-04-15 23:04:10 -0500

    Update Purchase.java

--- Version2/Purchase.java
+++ Version2/Purchase.java
@@ -1,6 +1,5 @@
 import java.io.Serializable;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 /**
  * Represents a single Transaction (purchase, renew, etc.)
@@ -8,67 +7,20 @@ import java.util.GregorianCalendar;
  * @modelName Brahma Dathan
  *
  */
-public class Transaction implements Serializable {
+public class Purchase implements Serializable {
     private static final long serialVersionUID = 1L;
-    private String type;
-    private String brandName;
-    private Calendar date;
+    private double price ; 
 
-    /**
-     * Creates the transaction with a given type and appliance brandName. The date is the
-     * current date.
-     * 
-     * @param type
-     *            The type of transaction
-     * @param brandName
-     *            The brandName of the appliance
-     * 
-     */
-    public Transaction(String type, String brandName) {
-        this.type = type;
-        this.brandName = brandName;
-        date = new GregorianCalendar();
+
+    public Purchase(String applianceId, String customerId, int quantity) {
+      this.applianceId = applianceId; 
+      this.customerId = customerId; 
+      
+      this.price = quantity // something goes here with multiplication - price for given applianceId
     }
 
-    /**
-     * Checks whether this transaction is on the given date
-     * 
-     * @param date
-     *            The date for which transactions are being sought
-     * @return true iff the dates match
-     */
-    public boolean onDate(Calendar date) {
-        return ((date.get(Calendar.YEAR) == this.date.get(Calendar.YEAR))
-                && (date.get(Calendar.MONTH) == this.date.get(Calendar.MONTH))
-                && (date.get(Calendar.DATE) == this.date.get(Calendar.DATE)));
-    }
 
-    /**
-     * Returns the type field
-     * 
-     * @return type field
-     */
-    public String getType() {
-        return type;
-    }
 
-    /**
-     * Returns the brandName field
-     * 
-     * @return brandName field
-     */
-    public String getBrandName() {
-        return brandName;
-    }
-
-    /**
-     * Returns the date as a String
-     * 
-     * @return date with month, date, and year
-     */
-    public String getDate() {
-        return date.get(Calendar.MONTH) + "/" + date.get(Calendar.DATE) + "/" + date.get(Calendar.YEAR);
-    }
 
     /**
      * String form of the transaction
