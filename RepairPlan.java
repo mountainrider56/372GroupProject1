f80ff4b7ccfa57852eff93f767cab30729d1713a
aaronzech <aaronzech@gmail.com>
2020-04-16 22:28:53 -0500

    Aaron's Changes
    Aaron's changes for repair plan class

--- RepairPlan.java
+++ RepairPlan.java
@@ -1,4 +1,34 @@
 
 public class RepairPlan {
 
+	private int cost;
+	private Customer c;
+	private Appliance a;
+	
+	public RepairPlan(int cost, Customer c, Appliance a) {
+		super();
+		this.cost = cost;
+		this.c = c;
+		this.a = a;
+	}
+	public int getCost() {
+		return cost;
+	}
+	public void setCost(int cost) {
+		this.cost = cost;
+	}
+	public Customer getC() {
+		return c;
+	}
+	public void setC(Customer c) {
+		this.c = c;
+	}
+	public Appliance getA() {
+		return a;
+	}
+	public void setA(Appliance a) {
+		this.a = a;
+	}
+	
+
 }
